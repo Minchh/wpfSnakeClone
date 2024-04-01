de02dc90e8c7308c8df4ded60480ccfe385368e2
Minchh <minh.nguyendo3103@gmail.com>
2024-04-02 02:20:28 +0700

    build: added GameState class (control the game mechanics)

---
+++ SnakeWPF/GameState.cs
@@ -0,0 +1,33 @@
+﻿namespace SnakeWPF
+{
+    public class GameState
+    {
+        public int Rows { get; }
+        public int Cols { get; }
+        public GridValue[,] Grid { get; }
+        public Direction Dir { get; private set; }
+        public int Score { get; private set; }
+        public bool GameOver { get; private set; }
+
+        private readonly LinkedList<Position> _snakePositions = new();
+        private readonly Random _random = new Random();
+
+        public GameState(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            Grid = new GridValue[Rows, Cols];
+            Dir = Direction.Right;
+        }
+
+        private void AddSnake()
+        {
+            int r = Rows / 2;
+            for (int c = 1; c <= 3; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+                _snakePositions.AddFirst(new Position(r, c));
+            }
+        }
+    }
+}
