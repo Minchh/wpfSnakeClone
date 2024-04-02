977b920d53be4983a8f839d7a177245e77f131d2
Minch <minh.nguyendo3103@gmail.com>
2024-04-02 16:54:22 +0700

    build: setup xaml resources, setup grid images, score text and grid xaml structure

--- SnakeWPF/MainWindow.xaml.cs
+++ SnakeWPF/MainWindow.xaml.cs
@@ -16,9 +16,37 @@ namespace SnakeWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly int _rows = 15, _cols = 15;
+        private readonly Image[,] _gridImages;
+
         public MainWindow()
         {
             InitializeComponent();
+            _gridImages = SetupGrid();
+
+        }
+
+        private Image[,] SetupGrid()
+        {
+            Image[,] images = new Image[_rows, _cols];
+            GameGrid.Rows = _rows;
+            GameGrid.Columns = _cols;
+
+            for (int r = 0; r < _rows; r++)
+            {
+                for (int c = 0; c < _cols; c++)
+                {
+                    Image image = new Image
+                    {
+                        Source = Images.Empty
+                    };
+
+                    images[r, c] = image;
+                    GameGrid.Children.Add(image);
+                }
+            }
+
+            return images;
         }
     }
 }
